2e7d433e976f48d53e5895f1e8c40cc1c288d582
Krimo Hafsaoui <krimo.hafsaoui@misterassur.com>
2013-04-09 17:00:28 +0200

    adding web service

---
+++ process.php
@@ -0,0 +1,101 @@
+<?php
+	
+	/**
+	 * Formatage de la date pour entrée dans la base de donnée
+	 * @param  string $date La date sous le format JJ/MM/AAAA
+	 * @return string       La date au format AAAA-MM-JJ
+	 */
+	function format_date($date) {
+		$date_arr = explode("/", filter_var($date, FILTER_SANITIZE_STRING));
+		$a = $date_arr[0];
+		$date_arr[0] = $date_arr[1];
+		$date_arr[1] = $a;
+		return date('Y-m-d',strtotime(implode("/",$date_arr)));
+	}
+
+	/**
+	 * Numero mobile ou non
+	 * @param  string $phone Le numéro à vérifier
+	 * @return boolean       Le retour vrai ou faux
+	 */
+	function check_mobile($phone) {
+		if (preg_match("/^0(6|7)/", $phone)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// Constantes
+	define("WS_URL", "http://dev.misterassur.com/moteur/moteur_import.php");
+	define("SERVICE", "animaux");
+
+	// Traitement variables
+	// (sanitanisation && renommage)
+	$animal_type = (isset($_POST['dog_breed'])) ? 1 : 2;
+	if (isset($_POST['dog_breed'])) {
+		$dog_breed = filter_var($_POST['dog_breed'], FILTER_SANITIZE_NUMBER_INT);
+	}
+	if (isset($_POST['cat_breed'])) {
+		$cat_breed = filter_var($_POST['cat_breed'], FILTER_SANITIZE_NUMBER_INT);
+	}
+	$breed = (isset($dog_breed)) ? $dog_breed : $cat_breed;
+	$pet_gender = ($_POST['pet_gender'] == "male") ? 1 : 2;
+	$pet_name = filter_var($_POST['pet_name'], FILTER_SANITIZE_STRING);
+	$pet_birthday = format_date($_POST['pet_birthday']);
+	$pet_tag = ($_POST['pet_tag'] == "on") ? 1 : 0;
+	$owner_gender = filter_var($_POST['owner_gender'], FILTER_SANITIZE_NUMBER_INT);
+	$owner_surname = filter_var($_POST['owner_surname'], FILTER_SANITIZE_STRING);
+	$owner_name = filter_var($_POST['owner_name'], FILTER_SANITIZE_STRING);
+	$owner_address = filter_var($_POST['owner_address'], FILTER_SANITIZE_STRING);
+	$zip_code = filter_var($_POST['zip_code'], FILTER_SANITIZE_NUMBER_INT);
+	$owner_birthday = format_date($_POST['owner_birthday']);
+	$owner_phone = filter_var($_POST['owner_phone'], FILTER_SANITIZE_NUMBER_INT);
+	$mobile_phone = (check_mobile($owner_phone)) ? $owner_phone : '';
+	$landline = (!check_mobile($owner_phone)) ? $owner_phone : '';
+	$owner_email = filter_var($_POST['owner_phone'], FILTER_SANITIZE_EMAIL);
+	$optin = ($_POST['optin'] == "on") ? 1 : 0;
+	$pet_insured = filter_var($_POST['pet_insured'], FILTER_SANITIZE_NUMBER_INT);
+	$contract_cancelled = filter_var($_POST['contract_cancelled'], FILTER_SANITIZE_NUMBER_INT);
+	$contract_start_date = format_date($_POST['contract_start_date']);
+	$contract_type = filter_var($_POST['contract_type'], FILTER_SANITIZE_NUMBER_INT);
+
+	try{
+		$client = new SoapClient(null, array("uri" => WS_URL, "location" => WS_URL, "trace" => 1, "exceptions" => 1));
+
+		$data = array(
+			"code" => "IG9DGC", 
+			"animal_couvert" => $pet_insured,
+			"resiliation" => $contract_cancelled, 
+			"date_effet" => $contract_start_date, 
+			"formule_souhaitee" => $contract_type, 
+			"ref_apporteur" => '', 
+			"ani_1_type_espece" => $animal_type,
+			"ani_1_nom" => $pet_name, 
+			"ani_1_date_naissance" => $pet_birthday, 
+			"ani_1_sexe_animal" => $pet_gender, 
+			"ani_1_race" => $breed,
+			"ani_1_couleur_animal" => '',
+			"ani_1_animal_tatoue" => $pet_tag,
+			"ani_1_numero_tatouage" => '',
+			"civilite" => $owner_gender, 
+			"nom" => $owner_name, 
+			"prenom" => $owner_surname, 
+			"date_naissance" => $owner_birthday, 
+			"adresse" => $owner_address, 
+			"cp" => $zip_code, 
+			"insee" => '', 
+			"email" => $owner_email, 
+			"tel_mobile" => $mobile_phone, 
+			"tel_bureau" => '', 
+			"tel_domicile" => $landline, 
+			"situation_familiale" => '', 
+			"profession" => '', 
+			"emailing" => $optin
+			);
+
+		$return = $client->setDatasFromForm("misterassur", "misterassur", SERVICE, $data);
+
+	} catch (SoapFault $exception){
+		print_r($exception);
+	}
---
+++ synthese.php
@@ -0,0 +1,49 @@
+<?php require_once("process.php") ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+
+	<title>Assurance chien - Les résultats de votre comparaison</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<meta name="keywords" content="assurance santé, assurance chien" />
+	<meta name="description" content="Une assurance santé pour votre chien" />
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+	<link rel="stylesheet" href="css/app.css">
+
+	<!-- Google font -->
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,900,400italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="header-container">
+		<div class="container">
+			<div class="row-fluid">
+				<div class="span7">
+					<div class="hgroup">
+						<h1 class="header">Mon Assurance Chien <br> <small>Le comparateur de mutuelles chien et chat</small></h1>
+
+						<span class="label label-info">Gratuit, rapide &amp; sans engagement</span>
+					</div>		
+				</div>
+				<div class="span5">
+					<img src="img/dog-cat.png" alt="Chien et chat" class="header-img visible-phone">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row-fluid">			
+			<div class="span12">
+				<h2>Comparaison :</h2>
+				<?php echo $return->synthese; ?>
+			</div>
+		</div>	
+	</div>
+</body>
+</html>
